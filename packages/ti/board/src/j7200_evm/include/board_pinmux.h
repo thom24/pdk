219b97d370fd08cf39e9a0444a017ec0b62f0463
M V Pratap Reddy <x0257344@ti.com>
2019-12-24 02:38:23 +0530

    Board: Fix for J7200 EVM board library build error
     - J7200 EVM board library files are using pinmux files from J721E EVM board.
       Build for J7200 EVM board library is failing due to the pinmux file name changes
       in j721e evm board sources.

--- packages/ti/board/src/j7200_evm/include/board_pinmux.h
+++ packages/ti/board/src/j7200_evm/include/board_pinmux.h
@@ -45,8 +45,8 @@
 /*                             Include Files                                  */
 /* ========================================================================== */
 
-#include <ti/board/src/j721e_evm/include/pinmux.h>
-#include <ti/board/src/j721e_evm/AM7xxx_pinmux.h>
+#include <ti/board/src/j7200_evm/include/pinmux.h>
+#include <ti/board/src/j7200_evm/AM7xxx_pinmux.h>
 #include <ti/csl/csl_types.h>
 
 #ifdef __cplusplus
