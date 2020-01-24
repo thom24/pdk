thom24/pdk

commit 2a676a5fa238807a40d389cb780713c26329269a

j721e: boardcfg: Partition the CLEC IR resources
Update the board config for j721e to match with the
u-boot loaded board config.

This partitions the interrupt routers correctly between
A72 hosts and C7x hosts.

Signed-off-by: Nikhil Devshatwar <nikhil.nd@ti.com>

packages/ti/drv/sciclient/soc/V1/sciclient_defaultBoardcfg.c
@@ -663,10 +663,16 @@ __attribute__(( aligned(128), section(".boardcfg_data") )) =
             .host_id = TISCI_HOST_ID_ALL,
         },
         {
-            .num_resource = 56U,
+            .num_resource = 32U,
             .type = TISCI_RESASG_UTYPE(TISCI_DEV_COMPUTE_CLUSTER0_GIC500SS, TISCI_RESASG_SUBTYPE_COMPUTE_CLUSTER0_GIC500SS_SPI_IRQ_GROUP0_FROM_GPIOMUX_INTRTR0),
             .start_resource = 392U,
-            .host_id = TISCI_HOST_ID_ALL,
+            .host_id = TISCI_HOST_ID_A72_2,
+        },
+        {
+            .num_resource = 24U,
+            .type = TISCI_RESASG_UTYPE(TISCI_DEV_COMPUTE_CLUSTER0_GIC500SS, TISCI_RESASG_SUBTYPE_COMPUTE_CLUSTER0_GIC500SS_SPI_IRQ_GROUP0_FROM_GPIOMUX_INTRTR0),
+            .start_resource = 424U,
+            .host_id = TISCI_HOST_ID_A72_3,
         },
         {
             .num_resource = 64U,
@@ -681,9 +687,9 @@ __attribute__(( aligned(128), section(".boardcfg_data") )) =
             .host_id = TISCI_HOST_ID_ALL,
         },
         {
-            .num_resource = 60U,
+            .num_resource = 16U,
             .type = TISCI_RESASG_UTYPE(TISCI_DEV_COMPUTE_CLUSTER0_GIC500SS, TISCI_RESASG_SUBTYPE_COMPUTE_CLUSTER0_GIC500SS_SPI_IRQ_GROUP2_FROM_NAVSS0_INTR_ROUTER_0),
-            .start_resource = 672U,
+            .start_resource = 716U,
             .host_id = TISCI_HOST_ID_ALL,
         },
         {

commit 0b090633c7b58c28077a713e03810036885b4604

j721e: boardcfg: Remove extra entry for CLEC IR
The default boardconfig accepts only one entry per utype.
resasg_entry_count is fixated to TISCI_RESASG_UTYPE_CNT

This needs to be changed to specific host ownership.

For now, remove the extra entry and revisit this later.
Fixes: 2a676a5f

Signed-off-by: Nikhil Devshatwar <nikhil.nd@ti.com>

packages/ti/drv/sciclient/soc/V1/sciclient_defaultBoardcfg.c
@@ -669,12 +669,6 @@ __attribute__(( aligned(128), section(".boardcfg_data") )) =
             .host_id = TISCI_HOST_ID_A72_2,
         },
         {
-            .num_resource = 24U,
-            .type = TISCI_RESASG_UTYPE(TISCI_DEV_COMPUTE_CLUSTER0_GIC500SS, TISCI_RESASG_SUBTYPE_COMPUTE_CLUSTER0_GIC500SS_SPI_IRQ_GROUP0_FROM_GPIOMUX_INTRTR0),
-            .start_resource = 424U,
-            .host_id = TISCI_HOST_ID_A72_3,
-        },
-        {
             .num_resource = 64U,
             .type = TISCI_RESASG_UTYPE(TISCI_DEV_COMPUTE_CLUSTER0_GIC500SS, TISCI_RESASG_SUBTYPE_COMPUTE_CLUSTER0_GIC500SS_SPI_IRQ_GROUP1_FROM_NAVSS0_INTR_ROUTER_0),
             .start_resource = 448U,