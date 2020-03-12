a738e9ea54be6c1a59ef015f899bc7677da35bd8
Piyali Goswami <piyali_g@ti.com>
2020-03-12 12:22:40 +0530

    PDK-4963: Fix for C++ build
    Suppress the declared but never referenced warning as the variables in these files are not expected to be used as a variable but used as a binary blob.
    
    Signed-off-by: Piyali Goswami <piyali_g@ti.com>

--- packages/ti/drv/sciclient/soc/V1/sciclient_defaultBoardcfg_pm_hex.h
+++ packages/ti/drv/sciclient/soc/V1/sciclient_defaultBoardcfg_pm_hex.h
@@ -48,7 +48,7 @@
 /*                           Macros & Typedefs                                */
 /* ========================================================================== */
 
-#define SCICLIENT_BOARDCFG_PM_SIZE_IN_BYTES (0U)
+#define SCICLIENT_BOARDCFG_PM_SIZE_IN_BYTES (2U)
 
 /* ========================================================================== */
 /*                         Structure Declarations                             */
@@ -66,5 +66,6 @@
 /*                            Global Variables                                */
 /* ========================================================================== */
 
-#define SCICLIENT_BOARDCFG_PM {\
-} /* 0 bytes */
+#define SCICLIENT_BOARDCFG_PM { \
+    0x00000100U,\
+} /* 2 bytes */
