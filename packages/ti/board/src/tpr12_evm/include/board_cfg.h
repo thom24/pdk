51d7c29c3950412dc2861cc0b11c10f868278e11
M V Pratap Reddy <x0257344@ti.com>
2020-06-01 02:38:14 +0530

    PDK-5038: Board: Updated TPR12 EVM board library
     - Updated board configurations
     - Added board utils

--- packages/ti/board/src/tpr12_evm/include/board_cfg.h
+++ packages/ti/board/src/tpr12_evm/include/board_cfg.h
@@ -49,52 +49,98 @@ extern "C" {
 #define BOARD_INFO_CPU_NAME     "tpr12"
 #define BOARD_INFO_BOARD_NAME   "tpr12_evm"
 
+/* UART LLD instance number for MSS UART port */
+#define BOARD_MSS_UART_INSTANCE                 (0U)
+
+/* UART LLD instance number for DSS UART port */
+#define BOARD_DSS_UART_INSTANCE                 (2U)
+
+/* UART LLD instance number for RCSS UART port */
+#define BOARD_RCSS_UART_INSTANCE                (3U)
+
 /* UART LLD instance number for primary UART port */
-#define BOARD_UART_INSTANCE                     (0U)
+#define BOARD_UART_INSTANCE                     (BOARD_MSS_UART_INSTANCE)
 
-/* I2C instance as master for master/slave test example */
-#define BOARD_I2C_MASTER_INSTANCE       	(0U)
-
-/* I2C instance as salve for master/slave test example */
-#define BOARD_I2C_SLAVE_INSTANCE		(0U)
-
-/* I2C instance connected to PMIC - WKUP I2C0 */
-#define BOARD_I2C_PMIC_INSTANCE                 (0U)
-
-/* I2C slave address of PMIC */
-#define BOARD_I2C_PMIC_ADDR                     (0U)
+/* SPI instance connected to PMIC - MSS SPIB */
+#define BOARD_SPI_PMIC_INSTANCE                 (1U)
 
 /* QSPI instance connected to OSPI NOR flash */
 #define BOARD_QSPI_NOR_INSTANCE                 (0U)
 
 /* Number of LEDS connected to GPIO */
-#define BOARD_GPIO_LED_NUM                      (0U)
+#define BOARD_GPIO_LED_NUM                      (1U)
 
-#define BOARD_MCU_EMAC_PORT_MAX                 (0x1U)
-#define BOARD_MCU_ETH_PORT                      (0x1U)
+/* GPIO instance connected to LED */
+#define BOARD_GPIO_LED_PORT_NUM                 (0U)
 
-/* MCU EMAC PHY MDIO address */
-#define BOARD_MCU_EMAC_PHY_ADDR                 (0U)
+/* GPIO pin connected to LED */
+#define BOARD_GPIO_LED_PIN_NUM                  (2U)
 
-/* MCU EMAC MAX REG DUMP */
-#define BOARD_MCU_EMAC_REG_DUMP_MAX             (16U)
+/* GPIO instance connected to push button */
+#define BOARD_GPIO_SWITCH_PIN_NUM               (28U)
 
-/* MCU EMAC PHY register address definitions for reading strap values */
-#define BOARD_MCU_EMAC_STRAP_STS1_ADDR          (0x6EU)
-#define BOARD_MCU_EMAC_STRAP_STS2_ADDR          (0x6FU)
+/* MSS EMAC Port number */
+#define BOARD_EMAC_PORT_MAX                     (0x1U)
+#define BOARD_ETH_PORT                          (0x0U)
 
-/* MCU_ETH1_INTn */
-#define BOARD_GPIO_MCU_EMAC_INT_PORT_NUM        (0)
-#define BOARD_GPIO_MCU_EMAC_INT_PIN_NUM         (0)
+/* MSS EMAC PHY MDIO address */
+#define BOARD_EMAC_PHY_ADDR                     (0U)
+
+/* MSS EMAC MAX REG DUMP */
+#define BOARD_EMAC_REG_DUMP_MAX                 (16U)
+
+/* MSS EMAC PHY register address definitions for reading strap values */
+#define BOARD_EMAC_STRAP_STS1_ADDR              (0x6EU)
+#define BOARD_EMAC_STRAP_STS2_ADDR              (0x6FU)
+
+/* MSS_ETH_INTn */
+#define BOARD_GPIO_EMAC_INT_PIN_NUM             (2U)        /* MSS GPIO_2 */
+
+/* MSS_ETH_INTn */
+#define BOARD_GPIO_EMAC_RST_PIN_NUM             (28U)        /* MSS GPIO_28 */
 
 /* Temperature sensor i2c instance */
 #define BOARD_TEMP_SENSOR_I2C_INSTANCE          (0U)
 
-/* Temperature sensor slave device addresses */
-#define BOARD_TEMP_SENSOR_I2C_SLAVE_DEVICE1_ADDR    (0x0)
-#define BOARD_TEMP_SENSOR_I2C_SLAVE_DEVICE2_ADDR    (0x0)
+/* Temperature sensor slave device address */
+#define BOARD_TEMP_SENSOR_I2C_SLAVE_DEVICE_ADDR (0x49U)
 
-#define BOARD_I2C_CURRENT_MONITOR_INSTANCE          (0U)
+#define BOARD_I2C_CURRENT_MONITOR_INSTANCE      (0U)
+
+/* I2C instance connected to EEPROM */
+#define BOARD_I2C_EEPROM_INSTANCE               (0U)
+
+/* I2C address for EEPROM */
+#define BOARD_I2C_EEPROM_ADDR                   (0x50U)
+
+/* Number of TPR12 boards supported */
+#define BOARD_ID_MAX_BOARDS                     (0x1U)
+
+/* EEPROM board ID information */
+#define BOARD_EEPROM_HEADER_FIELD_SIZE          (7U)
+#define BOARD_EEPROM_TYPE_SIZE                  (1U)
+#define BOARD_EEPROM_STRUCT_LENGTH_SIZE         (2U)
+#define BOARD_EEPROM_MAGIC_NUMBER               (0xEE3355AA)
+
+#define BOARD_BOARD_FIELD_TYPE                  (0x10)
+#define BOARD_MACINFO_FIELD_TYPE                (0x13)
+#define BOARD_ENDLIST                           (0xFE)
+
+#define BOARD_EEPROM_HEADER_LENGTH              (4U)
+#define BOARD_EEPROM_BOARD_NAME_LENGTH          (8U)
+#define BOARD_EEPROM_VERSION_LENGTH             (4U)
+#define BOARD_EEPROM_SERIAL_NO_LENGTH           (12U)
+#define BOARD_EEPROM_CONFIG_LENGTH              (32U)
+
+#define BOARD_EEPROM_BOARD_NAME_ADDR            (BOARD_EEPROM_HEADER_ADDR + BOARD_EEPROM_HEADER_LENGTH)
+#define BOARD_EEPROM_VERSION_ADDR               (BOARD_EEPROM_BOARD_NAME_ADDR + BOARD_EEPROM_BOARD_NAME_LENGTH)
+#define BOARD_EEPROM_SERIAL_NO_ADDR             (BOARD_EEPROM_VERSION_ADDR + BOARD_EEPROM_VERSION_LENGTH)
+#define BOARD_EEPROM_CONFIG_ADDR                (BOARD_EEPROM_SERIAL_NO_ADDR + BOARD_EEPROM_SERIAL_NO_LENGTH)
+
+#define BOARD_EEPROM_HEADER_ADDR                (0U)
+
+/* Enable NOR flash driver */
+#define BOARD_NOR_FLASH_IN
 
 #ifdef __cplusplus
 }
