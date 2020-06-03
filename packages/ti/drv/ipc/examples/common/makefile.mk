b69ca09c65b6b87e5db52515f7daf462d223c5e8
Angela Stegmaier <angelabaker@ti.com>
2020-06-02 19:18:52 -0500

    PDK-6738: IPC: echo_testb: Copy vecs for ipc_echo_testb sysbios example
    The latest sysbios expects the vecs to be present at address 0x0.
    When the ipc_echo_testb starts from BTCM from SPL branch-only mode
    for mcu1_0, the vecs at 0x0 are not matching the sysbios vecs.
    
    This patch adds a function to the sysbios reset functions to
    copy the vecs to 0x0 during bios startup for the btcm image for
    mcu1_0. With this, the vecs are as expected for sysbios and the image
    is able to run sucessfully and finish the echo test, even when
    run in branch-only mode.
    
    Signed-off-by: Angela Stegmaier <angelabaker@ti.com>

--- packages/ti/drv/ipc/examples/common/makefile.mk
+++ packages/ti/drv/ipc/examples/common/makefile.mk
@@ -31,7 +31,12 @@ ifeq ($(SOC),$(filter $(SOC), j721e j7200))
   XDC_CFG_UPDATE_$(CORE) = $(PDK_INSTALL_PATH)/ti/drv/ipc/examples/common/j721e/ipc_override.cfg
   ifeq ($(ECHO_TEST_BTCM), 1)
     ifeq ($(ISA), r5f)
-	  XDC_CFG_FILE_$(CORE) = $(PDK_INSTALL_PATH)/ti/drv/ipc/examples/common/j721e/sysbios_$(ISA).cfg
+      ifeq ($(CORE),mcu1_0)
+	    XDC_CFG_FILE_$(CORE) = $(PDK_INSTALL_PATH)/ti/drv/ipc/examples/common/j721e/sysbios_$(ISA)_btcm.cfg
+	    SRCS_ASM_COMMON += ipcCopyVecs2Exc.asm
+      else
+	    XDC_CFG_FILE_$(CORE) = $(PDK_INSTALL_PATH)/ti/drv/ipc/examples/common/j721e/sysbios_$(ISA).cfg
+      endif
 	  EXTERNAL_LNKCMD_FILE_LOCAL = $(PDK_INSTALL_PATH)/ti/drv/ipc/examples/common/j721e/linker_$(ISA)_$(CORE)_btcm_sysbios.lds
     else
 	  EXTERNAL_LNKCMD_FILE_LOCAL = $(PDK_INSTALL_PATH)/ti/drv/ipc/examples/common/j721e/linker_$(ISA)_$(CORE)_sysbios.lds
