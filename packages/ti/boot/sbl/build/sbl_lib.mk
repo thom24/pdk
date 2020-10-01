189d7e6335ef910e5c33511c33e67c2dc0c64f03
Badri S <badri@ti.com>
2020-10-01 14:10:28 +0530

    [PDK-7488] TPR12: SBL build fix and bios cfg file cleanup
    Build fix for SBL for TPR12 and cleanup sysbios cfg file
    as sysbios now support correct values for CPU freq for
    DSP/R5F and RTI timer
    
    Signed-off-by: Badri S <badri@ti.com>

--- packages/ti/boot/sbl/build/sbl_lib.mk
+++ packages/ti/boot/sbl/build/sbl_lib.mk
@@ -80,7 +80,6 @@ PACKAGE_SRCS_COMMON += ./.gitignore ./sbl_component.mk ./makefile ./sbl_ver.h
 SRCS_COMMON = sbl_soc.c
 SRCS_COMMON += sbl_rprc.c
 SRCS_COMMON += sbl_slave_core_boot.c
-SRCS_COMMON += sbl_qos.c
 SRCS_COMMON += UART_soc.c
 
 ifeq ($(SOC),$(filter $(SOC), tpr12))
@@ -88,6 +87,8 @@ SRCS_COMMON += sbl_utils_addrxlate.c
 SRCS_COMMON += sbl_csl.c
 SRCS_COMMON += sbl_rcm.c
 else
+#SBL_qos not applicable to TPR12
+SRCS_COMMON += sbl_qos.c
 SRCS_COMMON += sbl_sci_client.c
 SRCS_COMMON += sbl_vid_map.c
 endif
