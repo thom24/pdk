54891a8f5d44606f17bf8a45477eeddf38846fc8
Sam Nelson <sam.nelson@ti.com>
2020-10-13 09:29:08 -0400

    sdr_test: Add missed files
    Signed-off-by: Sam Nelson <sam.nelson@ti.com>

---
+++ packages/ti/diag/sdr/test/sdtf-test/src/j721e_evm/sdtf_soc.h
@@ -0,0 +1,104 @@
+/*
+ * SDR TEST
+ *
+ * Software Diagnostics Reference Test
+ *
+ *  Copyright (c) Texas Instruments Incorporated 2018-2020
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef INCLUDE_SDTF_SOC_H_
+#define INCLUDE_SDTF_SOC_H_
+
+
+SDR_ESM_InitConfig_t SDTF_esmInitConfig_MCU =
+{
+    .esmErrorConfig = {0u, 3u}, /* Self test error config */
+    .eventMap = {0xffffffffu, 0xff0fffffu, 0x7fffffffu, 0x00000007u,
+                },
+     /**< All events enable: except timer and self test  events, and Main ESM output */
+    /* Temporarily disabling vim compare error as well*/
+    .eventPriorityMap = {0xffffffffu, 0xff0fffffu, 0x7fffffffu, 0x00000007u,
+                        },
+    /**< All events high priority: except timer, selftest error events, and Main ESM output */
+    .errorOutputMap = {0xffffffffu, 0xff0fffffu, 0x7fffffffu, 0x00000007u,
+                      },
+    /**< All events high priority: except timer, selftest error events, and Main ESM output */
+};
+
+SDR_ESM_InitConfig_t SDTF_esmInitConfig_WKUP =
+{
+    .esmErrorConfig = {0u, 8u}, /* Self test error config */
+    .eventMap = {0xffffffffu, 0x00180003u, 0xffffffffu,
+                },
+     /**< All events enable: except clkstop events for unused clocks */
+    .eventPriorityMap = {0xffffffffu, 0x00180003u, 0xffffffffu,
+                        },
+    /**< All events high priority: except clkstop events for unused clocks */
+    .errorOutputMap = {0xffffffffu, 0x00180003u, 0xffffffffu, 
+                      },
+    /**< All events high priority: except clkstop for unused clocks
+     *   and selftest error events */
+};
+
+SDR_ESM_InitConfig_t SDTF_esmInitConfig_MAIN =
+{
+    .esmErrorConfig = {1u, 8u}, /* Self test error config */
+    .eventMap = {0x00000000u, 0xfffffffbu, 0x7fffffffu, 0xffffffffu,
+                 0xffffffffu, 0xffffffffu, 0xffffffffu, 0xffffffffu,
+                 0xffffffffu, 0xffffffffu, 0xffffffffu, 0xffffffffu,
+                 0xffffffffu, 0xffffffffu, 0xffffffffu, 0x00000000u,
+                 0x00000000u, 0x00000000u, 0x00000000u, 0x00000000u,
+                 0xffffffffu,
+                },
+     /**< All events enable: except clkstop events for unused clocks
+      *   and PCIE events */
+    .eventPriorityMap = {0x00000000u, 0xfffffffbu, 0x7fffffffu, 0xffffffffu,
+                         0xffffffffu, 0xffffffffu, 0xffffffffu, 0xffffffffu,
+                         0xffffffffu, 0xffffffffu, 0xffffffffu, 0xffffffffu,
+                         0xffffffffu, 0xffffffffu, 0xffffffffu, 0x00000000u,
+                         0x00000000u, 0x00000000u, 0x00000000u, 0x00000000u,
+                         0xffffffffu,
+                        },
+    /**< All events high priority: except clkstop events for unused clocks
+     *   and PCIE events */
+    .errorOutputMap = {0x00000000u, 0xfffffffbu, 0x7fffffffu, 0xffffffffu,
+                       0xffffffffu, 0xffffffffu, 0xffffffffu, 0xffffffffu,
+                       0xffffffffu, 0xffffffffu, 0xffffffffu, 0xffffffffu,
+                       0xffffffffu, 0xffffffffu, 0xffffffffu, 0x00000000u,
+                       0x00000000u, 0x00000000u, 0x00000000u, 0x00000000u,
+                       0xffffffffu,
+                      },
+    /**< All events high priority: except clkstop for unused clocks
+     *   and PCIE events */
+};
+
+#endif /* INCLUDE_SDTF_SOC_H_ */
