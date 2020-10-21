d6e85f93c9d7749590975e7a845d831ca82c2979
Jonathan Bergsagel <jbergsagel@ti.com>
2020-10-21 12:38:32 -0500

    J721E: sciclient/sbl: BoardCfg_RM cleanup
    Go back to single BoardCfg_RM for J721E, since the "default" and
    "linux" versions are now the same.
    
    Reduces confusion with BoardCfg_RM handling code in the SBL.
    
    Signed-off-by: Jonathan Bergsagel <jbergsagel@ti.com>

--- packages/ti/boot/sbl/soc/k3/sbl_sci_client.c
+++ packages/ti/boot/sbl/soc/k3/sbl_sci_client.c
@@ -42,12 +42,10 @@
 #include <sbl_err_trap.h>
 #include <sbl_sci_client.h>
 
-#if defined(SBL_ENABLE_HLOS_BOOT)
-#if defined(SOC_J721E) || defined(SOC_AM65XX)
+#if defined(SBL_ENABLE_HLOS_BOOT) && defined(SOC_AM65XX)
 const uint32_t gSciclient_boardCfgLow_hlos_rm[(SCICLIENT_BOARDCFG_RM_LINUX_SIZE_IN_BYTES+3U)/4U]
     __attribute__(( aligned(128), section(".boardcfg_data") ))
     = SCICLIENT_BOARDCFG_RM_LINUX;
-#endif
 #endif
 
 #ifdef __cplusplus
@@ -148,12 +146,10 @@ void SBL_SciClientInit(void)
 
     status = Sciclient_getDefaultBoardCfgInfo(&boardCfgInfo);
 
-#if defined(SBL_ENABLE_HLOS_BOOT)
-#if defined(SOC_J721E) || defined(SOC_AM65XX)
+#if defined(SBL_ENABLE_HLOS_BOOT) && defined(SOC_AM65XX)
     /* Replace default Sciclient boardCfgLowRm with alternate version for HLOS boot */
 	boardCfgInfo.boardCfgLowRm     = &gSciclient_boardCfgLow_hlos_rm[0U];
 	boardCfgInfo.boardCfgLowRmSize = SCICLIENT_BOARDCFG_RM_LINUX_SIZE_IN_BYTES;
-#endif
 #endif
 
     if (status != CSL_PASS)
