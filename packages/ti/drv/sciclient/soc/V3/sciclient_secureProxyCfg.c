e8f21cc73b6af01ef25266823bb7ba4c50066bff
Sheng Zhao <shengzhao@ti.com>
2020-11-17 16:19:42 -0600

    PDK-8458: sciclient: update secure proxy config for AM64x
    The M4F core on AM64x needs RAT to access the secure proxy registers.
    The offset of 0x60000000 is used to convert from MAIN domain memory
    map to M4FSS memory map.
    
    Signed-off-by: Sheng Zhao <shengzhao@ti.com>

--- packages/ti/drv/sciclient/soc/V3/sciclient_secureProxyCfg.c
+++ packages/ti/drv/sciclient/soc/V3/sciclient_secureProxyCfg.c
@@ -42,6 +42,15 @@
 #include <ti/csl/soc.h>
 #include <ti/csl/csl_sec_proxy.h>
 
+#if defined (BUILD_M4F)
+/** The offset is determined in CSL's m4 startup.c */
+#define M4F_RAT_MAP_OFFSET 0x60000000U
+/** Convert to M4F rat mapping */
+#define M4F_RAT_MAP(_val)  ((_val) + M4F_RAT_MAP_OFFSET)
+#else
+#define M4F_RAT_MAP(_val)  (_val)
+#endif
+
 /* ========================================================================== */
 /*                            Global Variables                                */
 /* ========================================================================== */
@@ -50,13 +59,13 @@
 *       the sec_proxy IP */
 CSL_SecProxyCfg gSciclient_secProxyCfg =
 {
-    (CSL_sec_proxyRegs *)       CSL_DMASS0_SEC_PROXY_MMRS_BASE,
+    (CSL_sec_proxyRegs *)       M4F_RAT_MAP(CSL_DMASS0_SEC_PROXY_MMRS_BASE),
     /*< pSecProxyRegs */
-    (CSL_sec_proxy_scfgRegs *)  CSL_DMASS0_SEC_PROXY_SCFG_BASE,
+    (CSL_sec_proxy_scfgRegs *)  M4F_RAT_MAP(CSL_DMASS0_SEC_PROXY_SCFG_BASE),
     /*< pSecProxyScfgRegs */
-    (CSL_sec_proxy_rtRegs *)    CSL_DMASS0_SEC_PROXY_RT_BASE,
+    (CSL_sec_proxy_rtRegs *)    M4F_RAT_MAP(CSL_DMASS0_SEC_PROXY_RT_BASE),
     /*< pSecProxyRtRegs */
-    (uint64_t)                  CSL_DMASS0_SEC_PROXY_SRC_TARGET_DATA_BASE,
+    (uint64_t)                  M4F_RAT_MAP(CSL_DMASS0_SEC_PROXY_SRC_TARGET_DATA_BASE),
     /*< proxyTargetAddr */
     0
     /*< maxMsgSize */
