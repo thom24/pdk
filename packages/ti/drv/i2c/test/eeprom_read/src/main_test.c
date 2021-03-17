thom24/pdk

commit 0d59f6540bc1015b12b245b497f557b5efc8a533

[Bug Fix]Fixed issue with I2C tests failing on Main Domain R5 Cores
- Issue:
    - Tests fails on Main Domain MCU2_1 and MCU3_1 cores
- Root-cause:
    - Due to recent RM changes, interrupts reserved from Interrupt Routers for these core are changed
    - This was making TC time-out as no interrupts were triggered
- Resolution:
    - Assign interrupts as per new RM i.e. +128 for MCUx_1 cores from Main Domain

Signed-off-by: Vivek Dhande <a0132295@ti.com>

packages/ti/drv/i2c/test/eeprom_read/src/main_test.c
@@ -225,7 +225,14 @@ bool Board_initI2C(void)
          * Pulsar R5 core is on the Main domain, use the Main Pulsar
          * interrupt router
          */
-        i2c_cfg.intNum = I2C_INST_WKUP_I2C0_INT_NUM_MAIN;
+        if(info.cpuID == 0U)
+        {
+            i2c_cfg.intNum = I2C_INST_WKUP_I2C0_INT_NUM_MAIN;
+        }
+        else
+        {
+            i2c_cfg.intNum = I2C_INST_WKUP_I2C0_INT_NUM_MAIN + 128U;
+        }
     }
     else
     {

commit bb3c6f531a96425ff2d73f10c9f1e16c5be60338

PDK-9464 : J7200 : I2C Demo not functional on mcu 21
	Root cause : Wrong interrupt number is used for mcu 21.
		The commit 0d59f6540b introduced this issue

	Fix : Limited the interrupt offset to j721e alone

	Test : tested on j7200 mcu 21

Signed-off-by: sujith <sujith.s@ti.com>

packages/ti/drv/i2c/test/eeprom_read/src/main_test.c
@@ -129,11 +129,15 @@ extern char eepromData[I2C_EEPROM_RX_LENGTH];
 #if defined (SOC_J721E)
 /* By default for, first available output from IR */
 #define I2C_INST_WKUP_I2C0_INT_NUM_MAIN (CSLR_R5FSS0_CORE0_INTR_R5FSS0_INTROUTER0_OUTL_0)
+/* Interrupt reserved for Core ID 1. 128 is based on RM */
+#define I2C_INST_WKUP_I2C0_INT_OFFSET   (128U)
 #endif
 
 #if defined (SOC_J7200)
 /* Applicable for MCU 20/21 only */
 #define I2C_INST_WKUP_I2C0_INT_NUM_MAIN (CSLR_R5FSS0_CORE0_INTR_WKUP_I2C0_POINTRPEND_0)
+/* No interrupt router, its directly connected */
+#define I2C_INST_WKUP_I2C0_INT_OFFSET   (0U)
 #endif /* J7200 Specific */
 /**********************************************************************
  ************************** Internal functions ************************
@@ -215,6 +219,7 @@ bool Board_initI2C(void)
 #if defined (BUILD_MPU)
     i2c_cfg.intNum = CSLR_COMPUTE_CLUSTER0_GIC500SS_SPI_WKUP_I2C0_POINTRPEND_0;
 #endif
+
 #if defined (BUILD_MCU)
     CSL_ArmR5CPUInfo info;
 
@@ -231,7 +236,8 @@ bool Board_initI2C(void)
         }
         else
         {
-            i2c_cfg.intNum = I2C_INST_WKUP_I2C0_INT_NUM_MAIN + 128U;
+            i2c_cfg.intNum = I2C_INST_WKUP_I2C0_INT_NUM_MAIN +
+                                I2C_INST_WKUP_I2C0_INT_OFFSET;
         }
     }
     else
@@ -239,7 +245,8 @@ bool Board_initI2C(void)
         /* Pulsar R5 core is on the MCU domain */
         i2c_cfg.intNum = CSLR_MCU_R5FSS0_CORE0_INTR_WKUP_I2C0_POINTRPEND_0;
     }
-#endif
+#endif /* for mcu builds only */
+
 #if defined (BUILD_C7X_1)
     i2c_cfg.eventId = CSLR_COMPUTE_CLUSTER0_GIC500SS_SPI_WKUP_I2C0_POINTRPEND_0 + 992U, /* eventId, input event # to CLEC */
 #endif