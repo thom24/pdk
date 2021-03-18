thom24/pdk

commit b431aefd763ff2f15468e87f67038ed5db55f50f

PDK-7060 : VCL OSAL updates
	OSAL UT works for mcu 10, mcu20 and mpu 10
	mcusw can_app works for mcu21

Signed-off-by: sujith <sujith.s@ti.com>

packages/ti/osal/soc/j7200/osal_soc.h
@@ -67,14 +67,15 @@ extern "C" {
 #define  TIMERP_TIMER_FREQ_LO   ((int32_t) 25000000)
 #define  TIMERP_TIMER_FREQ_HI   ((int32_t) 0)
 
-#define TimerP_numTimerDevices          ((uint32_t) 20U )
-#if defined (BUILD_MCU)
-  #define TIMERP_ANY_MASK               ((uint32_t) 0x000F)
-  #define TIMERP_AVAILABLE_MASK       ((uint32_t)(0x000F))
-#else
-  #define TIMERP_ANY_MASK               ((uint32_t) 0x0FFF)
-  #define TIMERP_AVAILABLE_MASK       ((uint32_t)(0x0FFF))
-#endif
+#define TimerP_numTimerDevices  (20U)
+/**< Total number of instances in a given domain */
+#define TIMERP_ANY_MASK         ((uint32_t) 0x1FFFFF)
+/**< Any available */
+#define TIMERP_AVAILABLE_MASK   ((uint32_t)(0x1FFFFF))
+/**< Which instances are available */
+#define TIMERP_INST_4_TS        (TimerP_getPreferredDefInst)
+
+
 
 /* using the default timer base addresses */
 #if defined(__aarch64__)
@@ -100,6 +101,14 @@ extern "C" {
 
 /* external references */
 extern Osal_HwAttrs  gOsal_HwAttrs;
+#if defined (BUILD_MCU)
+extern inline int32_t TimerP_getPreferredDefInst(void);
+#define OSAL_ARCH_TIMER_INST_FOR_TS (TimerP_getPreferredDefInst())
+/**< Returns the instance of timers required for given instance */
+#else
+#define OSAL_ARCH_TIMER_INST_FOR_TS (2)
+/**< Default timer for MPU */
+#endif
 
 #ifdef __cplusplus
 }

packages/ti/osal/soc/am335x/osal_soc.h
@@ -75,6 +75,9 @@ extern "C" {
 #define OSAL_TIRTOS_MAX_HWIP_PER_SOC         ((uint32_t) 10U)
 #define OSAL_TIRTOS_MAX_TIMERP_PER_SOC       (TimerP_numTimerDevices)
 
+#define OSAL_ARCH_TIMER_INST_FOR_TS         (TimerP_ANY)
+/**< Default timer instance for timer */
+
 #ifdef __cplusplus
 }
 #endif

packages/ti/osal/soc/am437x/osal_soc.h
@@ -75,6 +75,9 @@ extern "C" {
 #define OSAL_TIRTOS_MAX_HWIP_PER_SOC         ((uint32_t) 40U)
 #define OSAL_TIRTOS_MAX_TIMERP_PER_SOC       (TimerP_numTimerDevices)
 
+#define OSAL_ARCH_TIMER_INST_FOR_TS         (TimerP_ANY)
+/**< Default timer instance for timer */
+
 #ifdef __cplusplus
 }
 #endif

packages/ti/osal/soc/am571x/osal_soc.h
@@ -73,6 +73,8 @@ extern "C" {
 #define OSAL_TIRTOS_MAX_HWIP_PER_SOC         ((uint32_t) 40U)
 #define OSAL_TIRTOS_MAX_TIMERP_PER_SOC       (TimerP_numTimerDevices)
 
+#define OSAL_ARCH_TIMER_INST_FOR_TS         (TimerP_ANY)
+/**< Default timer instance for timer */
 
 #ifdef __cplusplus
 }

packages/ti/osal/soc/am572x/osal_soc.h
@@ -74,6 +74,9 @@ extern "C" {
 #define OSAL_TIRTOS_MAX_HWIP_PER_SOC         ((uint32_t) 40U)
 #define OSAL_TIRTOS_MAX_TIMERP_PER_SOC       (TimerP_numTimerDevices)
 
+#define OSAL_ARCH_TIMER_INST_FOR_TS         (TimerP_ANY)
+/**< Default timer instance for timer */
+
 #ifdef __cplusplus
 }
 #endif

packages/ti/osal/soc/am574x/osal_soc.h
@@ -73,6 +73,9 @@ extern "C" {
 #define OSAL_TIRTOS_MAX_HWIP_PER_SOC         ((uint32_t) 40U)
 #define OSAL_TIRTOS_MAX_TIMERP_PER_SOC       (TimerP_numTimerDevices)
 
+#define OSAL_ARCH_TIMER_INST_FOR_TS         (TimerP_ANY)
+/**< Default timer instance for timer */
+
 #ifdef __cplusplus
 }
 #endif

packages/ti/osal/soc/am64x/osal_soc.h
@@ -94,6 +94,9 @@ extern "C" {
 #define OSAL_TIRTOS_MAX_HWIP_PER_SOC         ((uint32_t) 40U)
 #define OSAL_TIRTOS_MAX_TIMERP_PER_SOC       (TimerP_numTimerDevices)
 
+#define OSAL_ARCH_TIMER_INST_FOR_TS         (TimerP_ANY)
+/**< Default timer instance for timer */
+
 /* external references */
 extern Osal_HwAttrs  gOsal_HwAttrs;
 

packages/ti/osal/soc/am65xx/osal_soc.h
@@ -89,6 +89,9 @@ extern "C" {
 #define OSAL_TIRTOS_MAX_HWIP_PER_SOC         ((uint32_t) 40U)
 #define OSAL_TIRTOS_MAX_TIMERP_PER_SOC       (TimerP_numTimerDevices)
 
+#define OSAL_ARCH_TIMER_INST_FOR_TS         (TimerP_ANY)
+/**< Default timer instance for timer */
+
 /* external references */
 extern Osal_HwAttrs  gOsal_HwAttrs;
 

commit bd801fb305b8c71b5b23c0e96aca36f794a1c20c

OSAL: C++ Build Fix: Resolve J7200 TimerP_getPreferredDefInst Build Errors
- Move extern inline Fxn definition of 'TimerP_getPreferredDefInst'
  from 'osal/soc/j7200/TimerP_default_r5f.c' to header file 'osal/soc/j7200/osal_soc.h'
- Fxn is refernced in Arch_utils.c,
  which will break C++ build if extern "inline" fxn defined in different source file
- Issue introduced by "b431aefd763ff2f15468e87f67038ed5db55f50f#packages/ti/osal/soc/j7200/osal_soc.h"

Signed-off-by: Don Dominic <a0486429@ti.com>

packages/ti/osal/soc/j7200/TimerP_default_r5f.c
@@ -242,23 +242,4 @@ void TimerP_updateDefaultInfoTbl(void)
     return;
 }
 
-inline int32_t TimerP_getPreferredDefInst(void)
-{
-    int32_t instVal;
-    CSL_ArmR5CPUInfo info;
-    CSL_armR5GetCpuID(&info);
-
-    /* Main domain R5F only */
-    instVal = 4;
-    if ((uint32_t)CSL_ARM_R5_CLUSTER_GROUP_ID_1 == info.grpId)
-    {
-        instVal = 6;
-    }
-    if (CSL_ARM_R5_CPU_ID_1 == info.cpuID)
-    {
-        instVal++;
-    }
-    return (instVal);
-}
-
 /* Nothing past this point */

packages/ti/osal/soc/j7200/osal_soc.h
@@ -45,6 +45,7 @@ extern "C" {
 #endif
 #include <ti/osal/osal.h>
 #include <ti/csl/soc.h>
+#include <ti/csl/arch/csl_arch.h>
 #if defined(TimerP_numTimerDevices)
 #undef  TimerP_numTimerDevices
 #endif
@@ -108,7 +109,24 @@ extern "C" {
 /* external references */
 extern Osal_HwAttrs  gOsal_HwAttrs;
 #if defined (BUILD_MCU)
-extern inline int32_t TimerP_getPreferredDefInst(void);
+inline int32_t TimerP_getPreferredDefInst(void)
+{
+    int32_t instVal;
+    CSL_ArmR5CPUInfo info;
+    CSL_armR5GetCpuID(&info);
+
+    /* Main domain R5F only */
+    instVal = 4;
+    if ((uint32_t)CSL_ARM_R5_CLUSTER_GROUP_ID_1 == info.grpId)
+    {
+        instVal = 6;
+    }
+    if (CSL_ARM_R5_CPU_ID_1 == info.cpuID)
+    {
+        instVal++;
+    }
+    return (instVal);
+}
 #define OSAL_ARCH_TIMER_INST_FOR_TS (TimerP_getPreferredDefInst())
 /**< Returns the instance of timers required for given instance */
 #else