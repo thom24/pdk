c69bd0e26859608f0e7d9d2ed313398181b55a1a
M V Pratap Reddy <x0257344@ti.com>
2021-04-30 20:03:55 +0530

    PDK-9638: Board: Enabled pinmux for different AWR2944 EVM board variants
     - AWR2944 EVM supports three board variants - ETS, LOP, LOP CZ.
       Pinmux is different between different variants of the board.
       Updated the board library to pick the pinmux file based on the build
       macro 'BOARD_VARIANT'.
       BOARD_VARIANT=AWR294X_ETS: Enables pinmux for ETS EVM
       BOARD_VARIANT=AWR294X_LOP: Enables pinmux for LOP EVM
       BOARD_VARIANT=AWR294X_LOP_CZ: Enables pinmux for LOP CZ EVM
    
       Default build is set for ETS EVM pinmux. Not specifying any value for
       BOARD_VARIANT will enable pinmux for ETS EVM

--- packages/ti/board/src/awr294x_evm/src_files_awr294x_evm.mk
+++ packages/ti/board/src/awr294x_evm/src_files_awr294x_evm.mk
@@ -3,4 +3,20 @@ SRCDIR += src/awr294x_evm src/awr294x_evm/include
 INCDIR += src/awr294x_evm src/awr294x_evm/include
 
 # Common source files across all platforms and cores
-SRCS_COMMON += board_init.c board_mmr.c board_pinmux.c board_pll.c board_clock.c board_utils.c board_info.c board_lld_init.c board_ethernet_config.c AWR294X_pinmux_data.c
+SRCS_COMMON += board_init.c board_mmr.c board_pinmux.c board_pll.c board_clock.c board_utils.c board_info.c board_lld_init.c board_ethernet_config.c
+
+ifeq ($(BOARD_VARIANT),)
+SRCS_COMMON += AWR294X_pinmux_data.c
+endif
+
+ifeq ($(BOARD_VARIANT), AWR294X_ETS)
+SRCS_COMMON += AWR294X_pinmux_data.c
+endif
+
+ifeq ($(BOARD_VARIANT),AWR294X_LOP)
+SRCS_COMMON += AWR294X_pinmux_data_lop.c
+endif
+
+ifeq ($(BOARD_VARIANT),AWR294X_LOP_CZ)
+SRCS_COMMON += AWR294X_pinmux_data_lopCZ.c
+endif
