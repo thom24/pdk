34a17d3739b5bad5571fee6cb9ea6b6364769063
Stephen Molfetta <sjmolfetta@ti.com>
2022-03-07 13:14:02 -0600

    keywriter: Add keywriter binary to the build dependencies
    Add the keywriter .bin file to the build dependencies and ensure that
    the keywriter binary data which is built as data into the keywriter
    application is regenerated in case the keywriter binary has been updated
    for the given SoC.
    
    Generate the keywriter C header into the soc-specific folder to avoid
    soc collisions.
    
    Signed-off-by: Stephen Molfetta <sjmolfetta@ti.com>

--- packages/ti/boot/keywriter/build/bin2c.mk
+++ packages/ti/boot/keywriter/build/bin2c.mk
@@ -1,7 +1,7 @@
-$(KEYWRITER_APP_DIR)/main.c: ti-fs-keywriter.h
+$(KEYWRITER_APP_DIR)/main.c: soc/$(SOC)/ti-fs-keywriter.h
 
-ti-fs-keywriter.h: $(BIN2C_EXE)
-	$(BIN2C_EXE) $(SIGNED_SYSFW_BIN) $(KEYWRITER_APP_DIR)/ti-fs-keywriter.h TIFS_KEYWRITER_BIN
+soc/$(SOC)/ti-fs-keywriter.h: tifs_bin/$(SOC)/ti-fs-keywriter.bin $(BIN2C_EXE)
+	$(BIN2C_EXE) $(SIGNED_SYSFW_BIN) $(KEYWRITER_APP_DIR)/soc/$(SOC)/ti-fs-keywriter.h TIFS_KEYWRITER_BIN
 
 $(BIN2C_EXE):
 	gcc $(SCI_CLIENT_TOOLS_BIN2C_DIR)/bin2c.c -o $(BIN2C_EXE)
