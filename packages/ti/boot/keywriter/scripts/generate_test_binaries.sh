0e3b84696239052c39baffeb31c6e34c90311057
Stephen Molfetta <sjmolfetta@ti.com>
2022-03-07 13:07:37 -0600

    keywriter: Update script to for cleaner test binary generation
    Update keywriter test binary generation script to define separate test
    cases for the defined set of tests (multi-pass, override negative check,
    one-shot programming) and place the binaries and logs into a unique
    directory.
    
    This facilitates automated running of the various tests.
    
    Signed-off-by: Stephen Molfetta <sjmolfetta@ti.com>

--- packages/ti/boot/keywriter/scripts/generate_test_binaries.sh
+++ packages/ti/boot/keywriter/scripts/generate_test_binaries.sh
@@ -16,52 +16,73 @@ if [ ! -f $PDK_KEYWR_COMP_PATH/scripts/ti_fek_public.pem ]; then
 	exit
 fi
 
-declare -A commands
-commands[1]="./gen_keywr_cert.sh --msv 0xC0FFE -t ti_fek_public.pem"
-commands[2]="./gen_keywr_cert.sh -t ti_fek_public.pem --sr-sbl 3 --sr-sysfw 1 "
-commands[3]="./gen_keywr_cert.sh -t ti_fek_public.pem --sr-bcfg 5"
-commands[4,1]="./construct_ext_otp_data.sh -extotp 0x80000001 -indx 0 -size 32"
-commands[4,2]="./gen_keywr_cert.sh -t ti_fek_public.pem --ext-otp ext_otp_data.bin --ext-otp-indx 0 --ext-otp-size 32 "
-commands[5,1]="./construct_ext_otp_data.sh -extotp 0x81 -indx 32 -size 8 " 
-commands[5,2]="./gen_keywr_cert.sh -t ti_fek_public.pem --ext-otp ext_otp_data.bin --ext-otp-indx 32 --ext-otp-size 8 "
-commands[6]="./gen_keywr_cert.sh -t ti_fek_public.pem -b keys/bmpk.pem --bmek keys/bmek.key -b-wp --bmek-wp --mek-opt 0x1 --mpk-opt 0x21"
-commands[7]="./gen_keywr_cert.sh -t ti_fek_public.pem -s keys/smpk.pem --smek keys/smek.key -s-wp --smek-wp --mek-opt 0x1 --mpk-opt 0x21"
-commands[8]="./gen_keywr_cert.sh -t ti_fek_public.pem --keycnt 2 --keycnt-wp "
-commands[9]="./gen_keywr_cert.sh -t ti_fek_public.pem --keyrev 1 "
+declare -A test1 test2 test3
+test1[comment]="Test 1 - multi-stage conversion"
+test1[1]="./gen_keywr_cert.sh --msv 0xC0FFE --msv-wp -t ti_fek_public.pem"
+test1[2]="./gen_keywr_cert.sh --msv 0xC0FFE --msv-ovrd -t ti_fek_public.pem"
+test1[3]="./gen_keywr_cert.sh -t ti_fek_public.pem --sr-sbl 3 --sr-sysfw 1 "
+test1[4]="./gen_keywr_cert.sh -t ti_fek_public.pem --sr-bcfg 5"
+test1[5]="./construct_ext_otp_data.sh -extotp 0x80000001 -indx 0 -size 32;
+        ./gen_keywr_cert.sh -t ti_fek_public.pem --ext-otp ext_otp_data.bin --ext-otp-indx 0 --ext-otp-size 32 "
+test1[6]="./construct_ext_otp_data.sh -extotp 0x81 -indx 32 -size 8;
+        ./gen_keywr_cert.sh -t ti_fek_public.pem --ext-otp ext_otp_data.bin --ext-otp-indx 32 --ext-otp-size 8 "
+test1[7]="./gen_keywr_cert.sh -t ti_fek_public.pem -b keys/bmpk.pem --bmek keys/bmek.key -b-wp --bmek-wp --mek-opt 0x1 --mpk-opt 0x21"
+test1[8]="./gen_keywr_cert.sh -t ti_fek_public.pem -s keys/smpk.pem --smek keys/smek.key -s-wp --smek-wp --mek-opt 0x1 --mpk-opt 0x21"
+test1[9]="./gen_keywr_cert.sh -t ti_fek_public.pem --keycnt 2 --keycnt-wp "
+test1[10]="./gen_keywr_cert.sh -t ti_fek_public.pem --keyrev 1 "
 
+test2[comment]="Test 2 - single-shot converstion"
+test2[1]="./gen_keywr_cert.sh -t ti_fek_public.pem
+            -s keys/smpk.pem --smek keys/smek.key -s-wp --smek-wp
+            -b keys/bmpk.pem --bmek keys/bmek.key -b-wp --bmek-wp
+            --mek-opt 0x1 --mpk-opt 0x21
+            --keycnt 2 --keycnt-wp
+            --keyrev 1" 
 
-commands[10]="/gen_keywr_cert.sh -t ti_fek_public.pem --msv 0x0001 "
-commands[11]="/gen_keywr_cert.sh -t ti_fek_public.pem --msv 0x0000 --msv-ovrd "
-commands[12]="/gen_keywr_cert.sh -t ti_fek_public.pem --msv 0x000F --msv-ovrd "
-commands[13]="/gen_keywr_cert.sh -t ti_fek_public.pem --msv 0x00FF --msv-ovrd --msv-wp "
-commands[14]="/gen_keywr_cert.sh -t ti_fek_public.pem --msv 0x0FFF --msv-ovrd "
+tests=(test1 test2)
 
 echo "make keywriter_img (initial)"
-make keywriter_img &>> $OUTPUT/initial_make.log
+make -C $PDK_INSTALL_PATH/ti/build keywriter_img
 
-for i in $(seq 1 14)
-do
-	echo "# Generating Binary No. ${i}"
-	cd $PDK_KEYWR_COMP_PATH/scripts
-	case "$i" in
-		"4"|"5")
-			echo " ${commands[$i,1]}"
-			${commands[$i,1]} &> $OUTPUT/kw_test${i}.log
-			echo " ${commands[$i,2]}"
-			${commands[$i,2]} &>> $OUTPUT/kw_test${i}.log
-			;;
-		*)
-			echo " ${commands[$i]}"
-			${commands[$i]} &> $OUTPUT/kw_test${i}.log
-			;;
-	esac
+function gen_tests()
+{
+    local -n test_array=$1
+    echo "Generating test seqeuence $2"
 
-	cp $PDK_KEYWR_COMP_PATH/x509cert/final_certificate.bin $OUTPUT/cert-kw_test${i}.bin
-	cd $PDK_INSTALL_PATH/ti/build
-	echo " make keywriter_img with TC ${i}"
-	make keywriter_img &>> $OUTPUT/kw_test${i}.log
-	cp $PDK_KEYWR_COMP_PATH/binary/${SOC}/keywriter_img_${SOC}_release.tiimage $OUTPUT/kw_test${i}.tiimage
-	cp $PDK_KEYWR_COMP_PATH/binary/${SOC}/keywriter_img_${SOC}_release.xer5f $OUTPUT/kw_test${i}.xer5f
+    # Create a unique test directory for storing the test binaries
+    TEST_OUT_DIR=$OUTPUT/test$2
+    mkdir -p $TEST_OUT_DIR
 
-	echo "# Done!"
+    # Dump the test description to into this directory
+    echo "${test_array[comment]}" > $TEST_OUT_DIR/test_description.txt
+
+    for i in $(seq 1 ${#test_array[@]})
+    do
+        i2=$(printf "%02d" $i)
+        cd $PDK_KEYWR_COMP_PATH/scripts
+        case "$i" in
+            "${#test_array[@]}")
+                # Don't generate the last iteration since there is a comment key
+                continue
+                ;;
+            *)
+                echo "${test_array[$i]}" |& tee -a $TEST_OUT_DIR/test_description.txt
+                eval ${test_array[$i]} &> $TEST_OUT_DIR/kw_test${i2}.log
+                ;;
+        esac
+
+        echo "# Generating test$2 Binary No. ${i}"
+        cp $PDK_KEYWR_COMP_PATH/x509cert/final_certificate.bin $TEST_OUT_DIR/cert-kw_test${i2}.bin
+        cd $PDK_INSTALL_PATH/ti/build
+        echo " make keywriter_img with TC ${i2}"
+        make keywriter_img &>> $TEST_OUT_DIR/kw_test${i2}.log
+        cp $PDK_KEYWR_COMP_PATH/binary/${SOC}/keywriter_img_${SOC}_release.tiimage $TEST_OUT_DIR/kw_test${i2}.tiimage
+        cp $PDK_KEYWR_COMP_PATH/binary/${SOC}/keywriter_img_${SOC}_release.xer5f  $TEST_OUT_DIR/kw_test${i2}.xer5f
+
+        echo "# Done!"
+    done
+}
+
+for i in $(seq 1 ${#tests[@]}); do
+    gen_tests ${tests[$((i-1))]} $i
 done
