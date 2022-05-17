a5c1f1847428ffcf20df9fa0d63ba68cd6a91f30
Rishabh Garg <rishabh@ti.com>
2022-05-17 14:31:02 +0530

    Fixed OSAL makefile
    - Added missing MCU and C7x cores for J7AEP/J7AHP
    - Removed xdc files from packaging
    
    Signed-off-by: Rishabh Garg <rishabh@ti.com>

--- packages/ti/osal/src/src_common_nonos.mk
+++ packages/ti/osal/src/src_common_nonos.mk
@@ -11,7 +11,7 @@ PACKAGE_SRCS_COMMON = makefile HwiP.h SwiP.h MuxIntcP.h osal.h osal_component.mk
                       src/nonos/MutexP_nonos.c src/src_common_nonos.mk src/Module.xs \
                       arch/core/Core_utils.c docs/OSAL_SoftwareManifest.html docs/ReleaseNotes_OSAL.pdf \
                       build \
-                      config_mk.bld osalver.h osalver.h.xdt package.bld package.xdc package.xs Settings.xdc.xdt
+                      config_mk.bld osalver.h
 
 
 ifeq ($(SOC),$(filter $(SOC),am571x am572x am574x am335x am437x k2h k2k k2e k2l k2g c6678 c6657 omapl137 omapl138 am65xx j721e am64x j721s2 j784s4))
@@ -35,7 +35,7 @@ ifeq ($(CORE),mpu1_0)
   PACKAGE_SRCS_COMMON += arch/core/a53
 endif
 
-ifeq ($(CORE),$(filter $(CORE), mcu1_0 mcu2_0 mcu2_1 mcu1_1))
+ifeq ($(CORE),$(filter $(CORE), mcu1_0 mcu2_0 mcu2_1 mcu1_1 mcu3_0 mcu3_1 mcu4_0 mcu4_1))
   SRCDIR += arch/core/r5
   SRCS_COMMON += CacheP_nonos.c Arch_util.c
   SRCS_ASM_COMMON += TimestampProvider_asm.asm
@@ -61,7 +61,7 @@ ifeq ($(CORE),$(filter $(CORE), c66x c66xdsp_1 c66xdsp_2 c674x))
   PACKAGE_SRCS_COMMON += arch/core/c6x src/nonos/EventCombinerP_nonos.c
 endif
 
-ifeq ($(CORE),$(filter $(CORE), c7x_1 c7x-hostemu))
+ifeq ($(CORE),$(filter $(CORE), c7x_1 c7x_2 c7x_3 c7x_4 c7x-hostemu))
   SRCDIR += arch/core/c7x
   SRCS_COMMON += CacheP_nonos.c Arch_util.c
   PACKAGE_SRCS_COMMON += arch/core/c7x
