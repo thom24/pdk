086e9c1ab7dd470baba0c10f65c88d5cf25c25c9
l-kumar <a0492259@ti.com>
2022-05-27 11:21:41 +0530

    J7AHP: UDMA: Remove HACK for Bord_init
    Fixes:PDK-12017
    
    Signed-off-by: l-kumar <a0492259@ti.com>

--- packages/ti/drv/udma/examples/udma_dru_direct_tr_test/main_rtos.c
+++ packages/ti/drv/udma/examples/udma_dru_direct_tr_test/main_rtos.c
@@ -73,13 +73,6 @@
 static void taskFxn(void* a0, void* a1);
 extern int32_t Udma_druDirectTrTest(void);
 
-/* HACK for J7AHP until BOARD support added*/
-#if defined(SOC_J784S4)
-Board_initCfg           boardCfg;
-Board_STATUS Board_init(Board_initCfg boardCfg){return 1;}
-#endif
-/*******************************************/
-
 /* ========================================================================== */
 /*                            Global Variables                                */
 /* ========================================================================== */
