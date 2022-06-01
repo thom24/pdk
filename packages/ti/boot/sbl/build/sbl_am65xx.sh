55b598fc1aaf1b95eb0715ba79fba755567b6bd6
Rishabh Garg <rishabh@ti.com>
2022-06-01 21:52:17 +0530

    [SBL] Removed files for legacy SOCs
    Signed-off-by: Rishabh Garg <rishabh@ti.com>

--- packages/ti/boot/sbl/build/sbl_am65xx.sh
+++
@@ -1,9 +0,0 @@
-#build instructions for bootloader
-
-ls
-
-make clean all
-
-#archive
-cd ..
-tar -cf sbl.tar --exclude='obj' --exclude='*.tar' --exclude='*.git*' ./*
--- packages/ti/boot/sbl/example/tpr12MulticoreApp/r4_tb/build/windows/setenvvar.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set XDC_INSTALL_PATH=C:/ti/xdctools_3_50_08_24_core
-set R4_CODEGEN_INSTALL_PATH=C:/git/tools/cgtools/arm/18.12.2.LTS
-set R4_TB_PATH=C:/git/mmwave_boot_test/r4_tb
