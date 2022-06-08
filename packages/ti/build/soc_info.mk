588f00819cd574ae62ea374289133fd892528d78
Rishabh Garg <rishabh@ti.com>
2022-06-08 20:10:24 +0530

    Fixed PDK build for SBL
    - Multicore apps fail for J784S4 as soc info is not in sync with sbl_component.mk
    - Last core in sbl_component.mk is mpu2_3 whereas it is specified in middle of soc_info.mk
    - Updated soc_info.mk to make mpu2_3 as last core
    
    Signed-off-by: Rishabh Garg <rishabh@ti.com>

--- packages/ti/build/soc_info.mk
+++ packages/ti/build/soc_info.mk
@@ -8,4 +8,4 @@ BOARD_LIST_j784s4 = j784s4_evm
 CORE_LIST_j721e    = mpu1_0 mcu1_0 mcu1_1 mcu2_0 mcu2_1 mcu3_0 mcu3_1 c66xdsp_1 c66xdsp_2 c7x_1 mpu1_1
 CORE_LIST_j7200    = mpu1_0 mcu1_0 mcu1_1 mcu2_0 mcu2_1 mpu1_1
 CORE_LIST_j721s2   = mpu1_0 mcu1_0 mcu1_1 mcu2_0 mcu2_1 mcu3_0 mcu3_1 c7x_1 c7x_2 mpu1_1
-CORE_LIST_j784s4   = mpu1_0 mpu1_1 mpu1_2 mpu1_3 mpu2_0 mpu2_1 mpu2_2 mpu2_3 mcu1_0 mcu1_1 mcu2_0 mcu2_1 mcu3_0 mcu3_1 mcu4_0 mcu4_1 c7x_1 c7x_2 c7x_3 c7x_4
+CORE_LIST_j784s4   = mpu1_0 mcu1_0 mcu1_1 mcu2_0 mcu2_1 mcu3_0 mcu3_1 mcu4_0 mcu4_1 c7x_1 c7x_2 c7x_3 c7x_4 mpu1_1 mpu1_2 mpu1_3 mpu2_0 mpu2_1 mpu2_2 mpu2_3
