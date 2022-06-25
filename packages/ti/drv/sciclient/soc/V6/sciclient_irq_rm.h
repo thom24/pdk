d8c18bb4669758dfa54646ccd9d88789c3e1b1ae
Sheng Zhao <shengzhao@ti.com>
2022-06-24 21:06:39 -0500

    j784s4: Update RM board config file
    SA3SS resources are removed.
    
    The RM board config is generated from an updated k3-resource-partitioning.
    Unused resources are removed to reduce the size of the RM board config,
    so the overall board config will fit within 0x1000.
    
    Fixes: SYSFW-5504
    
    Signed-off-by: Sheng Zhao <shengzhao@ti.com>

--- packages/ti/drv/sciclient/soc/V6/sciclient_irq_rm.h
+++ packages/ti/drv/sciclient/soc/V6/sciclient_irq_rm.h
@@ -50,7 +50,7 @@
 /* ========================================================================== */
 
 #if defined (SOC_J784S4)
-#define SCICLIENT_RM_IA_NUM_INST 5
+#define SCICLIENT_RM_IA_NUM_INST 4
 
 #define SCICLIENT_RM_IR_NUM_INST 8
 
