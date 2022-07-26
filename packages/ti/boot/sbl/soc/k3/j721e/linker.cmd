c0af32b3d6b1348fa9fe6c79109aa5761db2c6ad
Sai Ramakurthi <s-ramakurthi@ti.com>
2022-07-26 21:10:11 +0530

    [j721e] : increases the memory allocated to the SBL_SYSFW for j721e
    Signed-off-by: Sai Ramakurthi <s-ramakurthi@ti.com>

--- packages/ti/boot/sbl/soc/k3/j721e/linker.cmd
+++ packages/ti/boot/sbl/soc/k3/j721e/linker.cmd
@@ -48,13 +48,13 @@ MEMORY
      * after SYSFW is done.
      * Location of all board configurations: 0x41c80040 after SYSFW is done.
      */
-    OCMRAM_SBL_SYSFW (RWIX)   : origin=0x41C80000 length=0x40000
+    OCMRAM_SBL_SYSFW (RWIX)   : origin=0x41C80000 length=0x42000
 
     /* This is the maximum required by custom boot app. Do not use. */
-    OCMRAM_SBL_RESERVED_CUST_BOOT (RWIX)   : origin=0x41CC0000 length=0x40000 - 0x500
+    OCMRAM_SBL_RESERVED_CUST_BOOT (RWIX)   : origin=0x41CC2000 length=0x40000 - 0x2500
 
     /* X509 Header to be left behind by CCS Init for Sciserver to read */
-    OCMC_RAM_X509_HEADER (RWIX)     : ORIGIN = 0x41cffb00 , LENGTH = 0x500
+    OCMC_RAM_X509_HEADER (RWIX)     : ORIGIN = 0x41CFFB00 , LENGTH = 0x500
 
 }  /* end of MEMORY */
 
