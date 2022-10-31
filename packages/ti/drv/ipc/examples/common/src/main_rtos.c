cb6ed426b7c03f3cafdbb298b09382c0d2e405e4
Chandru Dhavamani <a0497642@ti.com>
2022-10-31 13:37:01 +0530

    Revert "HACK: J7AHP: Power On DRUs local to C7X cores"
    This reverts commit 82b0b94f2c51bb96c9f01ec2519491923aa128d2.
    
    By default local DRU will be powered ON.
    
    Signed-off-by: Chandru Dhavamani <a0497642@ti.com>

--- packages/ti/drv/ipc/examples/common/src/main_rtos.c
+++ packages/ti/drv/ipc/examples/common/src/main_rtos.c
@@ -102,61 +102,6 @@
  */
 #define IPC_SETUP_SCISERVER_TASK_PRI_LOW    (4)
 
-/* PSC defines*/
-/* This is a HACK to POWER ON DRU's local to C7x */
-#if defined ECHO_TEST_BTCM && defined FREERTOS && defined BUILD_MCU && defined SOC_J784S4
-#define BOLTON_PSC_BASE             (0x00420000U)
-
-#define PSC_MDCTL00                 (0xA00U)
-#define PSC_MDSTAT00                (0x800U)
-#define PSC_PDCTL00                 (0x300U)
-#define PSC_PDSTAT00                (0x200U)
-#define PSC_PTCMD                   (0x120U)
-#define PSC_PTSTAT                  (0x128U)
-#define PSC_PTCMD_H                 (0x124U)
-#define PSC_PTSTAT_H                (0x12CU)
-
-//PSC Parameter definitions
-#define PSC_PD_OFF                  (0x0U)
-#define PSC_PD_ON                   (0x1U)
-
-#define PSC_SYNCRESETDISABLE        (0x0U)
-#define PSC_SYNCRESET               (0x1U)
-#define PSC_DISABLE                 (0x2U)
-#define PSC_ENABLE                  (0x3U)
-
-#define BOLTON_PSC_MDCTL_BASE       (BOLTON_PSC_BASE + PSC_MDCTL00)
-#define BOLTON_PSC_MDSTAT_BASE      (BOLTON_PSC_BASE + PSC_MDSTAT00)
-#define BOLTON_PSC_PDCTL_BASE       (BOLTON_PSC_BASE + PSC_PDCTL00)
-#define BOLTON_PSC_PDSTAT_BASE      (BOLTON_PSC_BASE + PSC_PDSTAT00)
-#define BOLTON_PSC_PTCMD_BASE       (BOLTON_PSC_BASE + PSC_PTCMD)
-#define BOLTON_PSC_PTSTAT_BASE      (BOLTON_PSC_BASE + PSC_PTSTAT)
-#define BOLTON_PSC_PTCMD            (BOLTON_PSC_PTCMD_BASE)
-#define BOLTON_PSC_PTSTAT           (BOLTON_PSC_PTSTAT_BASE)
-#define BOLTON_PSC_PTCMD_H          (BOLTON_PSC_BASE + PSC_PTCMD_H)
-#define BOLTON_PSC_PTSTAT_H         (BOLTON_PSC_BASE + PSC_PTSTAT_H)
-
-#define PSC_TIMEOUT                 (1000U)
-
-#define BOLT_PD_ANA0                (4U)
-#define BOLT_PD_ANA1                (5U)
-#define BOLT_PD_ANA2                (6U)
-#define BOLT_PD_ANA3                (7U)
-
-#define LPSC_MSMC_L1_0              (14U)
-#define LPSC_DRU_0                  (15U)
-#define LPSC_ANA_PBIST_0            (16U)
-#define LPSC_MSMC_L1_1              (17U)
-#define LPSC_DRU_1                  (18U)
-#define LPSC_ANA_PBIST_1            (19U)
-#define LPSC_MSMC_L1_2              (20U)
-#define LPSC_DRU_2                  (21U)
-#define LPSC_ANA_PBIST_2            (22U)
-#define LPSC_MSMC_L1_3              (23U)
-#define LPSC_DRU_3                  (24U)
-#define LPSC_ANA_PBIST_3            (25U)
-#endif
-
 /* ========================================================================== */
 /*                         Structure Declarations                             */
 /* ========================================================================== */
@@ -207,101 +152,6 @@ extern Sciclient_ServiceHandle_t gSciclientHandle;
 /*                          Function Definitions                              */
 /* ========================================================================== */
 
-/* This is a HACK to POWER ON DRU's local to C7x */
-#if defined ECHO_TEST_BTCM && defined FREERTOS && defined BUILD_MCU && defined SOC_J784S4
-int32_t Board_SetPSCState(uint32_t pd_id, uint32_t md_id, uint32_t pd_state, uint32_t md_state)
-{
-    uint32_t regVal;
-    uint32_t mdctl;
-    uint32_t mdstat;
-    uint32_t pdctl;
-    uint32_t pdstat;
-    uint32_t ptcmd;
-    uint32_t ptstat;
-
-    uint32_t loop_cnt = 0;
-    int32_t ret = 0;
-
-    uint32_t address_offset = 0;
-
-    //Added for support beyond 32-domains
-    if(pd_id > 31){
-        ptcmd = BOLTON_PSC_PTCMD_H;
-        ptstat = BOLTON_PSC_PTSTAT_H;
-    }else{
-        ptcmd = BOLTON_PSC_PTCMD;
-        ptstat = BOLTON_PSC_PTSTAT;
-    }
-
-    mdctl = (BOLTON_PSC_MDCTL_BASE + ( 4U * md_id ) + address_offset) ;
-    mdstat = (BOLTON_PSC_MDSTAT_BASE + ( 4U * md_id )+ address_offset);
-    pdctl = (BOLTON_PSC_PDCTL_BASE + ( 4U * pd_id ) + address_offset);
-    pdstat = (BOLTON_PSC_PDSTAT_BASE + ( 4U * pd_id )+ address_offset);
-
-    // If state is already set, do nothing
-    if ( (( HW_RD_REG32(pdstat) & 0x1 ) == pd_state) && (( HW_RD_REG32(mdstat) & 0x1f ) == md_state) )
-    {
-        ret = -1;
-        return ret;
-    }
-
-    // Wait for GOSTAT to clear
-    while( (loop_cnt < PSC_TIMEOUT) && (HW_RD_REG32(ptstat+address_offset) & (0x1 << (pd_id % 32))) != 0 )
-    {
-        loop_cnt++;
-    }
-
-    // Check if we got timeout error while waiting
-    if (loop_cnt >= PSC_TIMEOUT)
-    {
-        ret = -1;
-        return ret;
-    }
-
-    // Set PDCTL NEXT to new state
-    regVal = (HW_RD_REG32(pdctl) & ~(0x1)) | pd_state;
-    HW_WR_REG32(pdctl, regVal);
-
-    // Set MDCTL NEXT to new state
-    regVal = (HW_RD_REG32(mdctl) & ~(0x1f)) | md_state;
-    HW_WR_REG32(mdctl, regVal);
-
-    // Start power transition by setting PTCMD GO to 1
-    regVal = (HW_RD_REG32(ptcmd+address_offset)) | (0x1<< (pd_id % 32));
-    HW_WR_REG32((ptcmd+address_offset), regVal);
-
-    loop_cnt = 0;
-
-    // Wait for PTSTAT GOSTAT to clear
-    while( (loop_cnt < PSC_TIMEOUT) && (HW_RD_REG32(ptstat+address_offset) & (0x1 << (pd_id % 32))) != 0 )
-    {
-        loop_cnt++;
-    }
-
-    // Check if we got timeout error while waiting
-    if (loop_cnt >= PSC_TIMEOUT)
-    {
-        ret = -1;
-        return ret;
-    }
-
-    // Verify power domain and module domain state got changed
-    if ( (( HW_RD_REG32(pdstat) & 0x1 ) == pd_state) && (( HW_RD_REG32(mdstat) & 0x1f ) == md_state) )
-    {
-        ret = 0;
-    }
-    if (( HW_RD_REG32(pdstat) & 0x1 ) != pd_state)
-    {
-        ret = -1;
-    }
-    if (( HW_RD_REG32(mdstat) & 0x1f ) != md_state)
-    {
-        ret = -1;
-    }
-
-    return ret;
-}
-#endif
 
 void ipc_initSciclient()
 {
@@ -495,14 +345,6 @@ void Ipc_setupSciServer(void *arg0, void *arg1)
     {
         App_printf("Starting Sciserver..... FAILED\n");
     }
-
-/* This is a HACK to POWER ON DRU's local to C7x */
-#if defined ECHO_TEST_BTCM && defined FREERTOS && defined BUILD_MCU && defined SOC_J784S4
-    Board_SetPSCState(BOLT_PD_ANA0,LPSC_DRU_0, PSC_PD_ON, PSC_ENABLE);
-    Board_SetPSCState(BOLT_PD_ANA1,LPSC_DRU_1, PSC_PD_ON, PSC_ENABLE);
-    Board_SetPSCState(BOLT_PD_ANA2,LPSC_DRU_2, PSC_PD_ON, PSC_ENABLE);
-    Board_SetPSCState(BOLT_PD_ANA3,LPSC_DRU_3, PSC_PD_ON, PSC_ENABLE);
-#endif
 
     return;
 }
