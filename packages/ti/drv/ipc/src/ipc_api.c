a9c8e4160d737b27be34fe8462fbe20e3cf19e08
Likhitha <x1097556@ti.com>
2022-11-26 14:49:13 +0530

    IPC: stack corruption of taskWaiter used in RPMessage_getRemoteEndPt
    Signed-off-by: Likhitha <x1097556@ti.com>

--- packages/ti/drv/ipc/src/ipc_api.c
+++ packages/ti/drv/ipc/src/ipc_api.c
@@ -120,6 +120,7 @@ typedef struct RPMessage_Waiter_s
     char               name[SERVICENAMELEN];
     uint32_t           token;
 } RPMessage_Waiter;
+
 /**
  *  \brief Element to hold payload copied onto receiver's queue.
  */
@@ -217,6 +218,9 @@ static int32_t RPMessage_processAnnounceMsg(RPMessage_Announcement *amsg, uint32
 static RPMessage_Module module;
 
 static Ipc_Object gIpcObject;
+
+RPMessage_Waiter  taskWaiter_pool[MAXENDPOINTS];
+
 /**< IPC Object */
 /* ========================================================================== */
 /*                          Function Definitions                              */
@@ -713,7 +717,7 @@ int32_t RPMessage_getRemoteEndPtToken(uint32_t selfProcId, const char* name, uin
     int32_t            rtnVal = IPC_SOK;
 #ifndef IPC_EXCLUDE_CTRL_TASKS
     void              *semHandle;
-    RPMessage_Waiter   taskWaiter;
+    RPMessage_Waiter  taskWaiter = taskWaiter_pool[*remoteEndPt];
 #endif /* IPC_EXCLUDE_CTRL_TASKS */
     size_t             namelen;
     Ipc_OsalPrms      *pOsalPrms = &gIpcObject.initPrms.osalPrms;
