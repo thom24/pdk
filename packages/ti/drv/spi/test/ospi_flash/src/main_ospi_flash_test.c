e1bd3438564bdb1f46602dcd72a9f5693f235598
Lohith Kumar <l-kumar@ti.com>
2023-02-10 13:51:22 +0530

    OSPI: Fix: Incorrect throughput values
    OSPI Flash Test reporting throughtpu of ~1 GBPS far more
    theoretical values when we use TimerP_getTimeInUsecs.
    Using GTC Timer instead for measuring performance.
    
    Signed-off-by: Lohith Kumar <l-kumar@ti.com>

--- packages/ti/drv/spi/test/ospi_flash/src/main_ospi_flash_test.c
+++ packages/ti/drv/spi/test/ospi_flash/src/main_ospi_flash_test.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 - 2021 Texas Instruments Incorporated - http://www.ti.com/
+ * Copyright (C) 2017 - 2023 Texas Instruments Incorporated - http://www.ti.com/
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -198,6 +198,21 @@ typedef struct OSPI_Tests_s
 #define OSPI_FLASH_SEL_NAND     (1U)
 #endif
 
+/* GTC Timer Definitions */
+#define OSPI_FLASH_GTC_MOD_ID           (TISCI_DEV_GTC0)
+#define OSPI_FLASH_GTC_CLK_ID           (TISCI_DEV_GTC0_GTC_CLK)
+#define OSPI_FLASH_GTC_CLK_FREQ         (200000000U)
+
+#define OSPI_FLASH_DEVICE_ID            (TISCI_DEV_MCU_FSS0_OSPI_0)
+#define OSPI_FLASH_RCLK_CLK             (TISCI_DEV_MCU_FSS0_OSPI_0_OSPI_RCLK_CLK)
+#define OSPI_FLASH_PARENT_CLK_166M      (TISCI_DEV_MCU_FSS0_OSPI_0_OSPI_RCLK_CLK_PARENT_HSDIV4_16FFT_MCU_2_HSDIVOUT4_CLK)
+#define OSPI_FLASH_PARENT_CLK_133M      (TISCI_DEV_MCU_FSS0_OSPI_0_OSPI_RCLK_CLK_PARENT_HSDIV4_16FFT_MCU_1_HSDIVOUT4_CLK)
+
+/* Get GTC Timer Ticks */
+#define Ospi_getGTCTimerTicks()         (*((uint64_t *)(CSL_GTC0_GTC_CFG1_BASE + 0x8U)))
+/* GTC ticks to Usecs */
+#define OSPI_GTC_TICKS_TO_USEC(X)       ((X * 1000000)/OSPI_FLASH_GTC_CLK_FREQ)
+
 /**********************************************************************
  ************************** Internal functions ************************
  **********************************************************************/
@@ -209,6 +224,16 @@ static void GeneratePattern(uint8_t *txBuf, uint8_t *rxBuf, uint32_t length);
 bool VerifyData(uint8_t *expData,
                 uint8_t *rxData,
                 uint32_t length);
+
+#ifdef OSPI_PROFILE
+static int32_t App_setGTCClk(uint32_t moduleId,
+                            uint32_t clkId,
+                            uint64_t clkRateHz);
+
+int32_t OspiFlash_ClkRateSet(uint32_t modId,
+                             uint32_t clkId,
+                             uint64_t clkRate);
+#endif
 
 /**********************************************************************
  ************************** Global Variables **************************
@@ -979,8 +1004,8 @@ static bool OSPI_flash_test(void *arg)
     uint32_t          testLen = TEST_DATA_LEN;
     uint32_t          testSegLen = TEST_XFER_SEG_LEN;
 #ifdef OSPI_PROFILE
-    uint64_t          startTime; /* start time stamp in usec */
-    uint64_t          elapsedTime; /* elapsed time in usec */
+    uint64_t          startTick; /* start GTC tick */
+    uint64_t          numTicks; /* numbet of GTC ticks */
     float             xferRate;
     uint32_t          xferRateInt;
 #if defined(FREERTOS)
@@ -1046,7 +1071,7 @@ static bool OSPI_flash_test(void *arg)
 
 #ifdef OSPI_PROFILE
     /* Get start time stamp for the Board_flashOpen measurement */
-    startTime = TimerP_getTimeInUsecs();
+    startTick = Ospi_getGTCTimerTicks();
 #endif
     /* Open the Board OSPI NOR device with OSPI port 0
        and use default OSPI configurations */
@@ -1055,8 +1080,8 @@ static bool OSPI_flash_test(void *arg)
 
 #ifdef OSPI_PROFILE
     /* Get elapsed time for the Board_flashOpen measurement */
-    elapsedTime = TimerP_getTimeInUsecs() - startTime;
-    SPI_log("\n Board_flashOpen elpased time %d us \n", (uint32_t)elapsedTime);
+    numTicks = Ospi_getGTCTimerTicks() - startTick;
+    SPI_log("\n Board_flashOpen elpased time %d us \n", (uint32_t)OSPI_GTC_TICKS_TO_USEC(numTicks));
 #endif
     if (!boardHandle)
     {
@@ -1142,7 +1167,7 @@ static bool OSPI_flash_test(void *arg)
     LoadP_reset( );
 #endif
     /* Get start time stamp for the write performance measurement */
-    startTime = TimerP_getTimeInUsecs();
+    startTick = Ospi_getGTCTimerTicks();
 #endif
     for (i = 0; i < testLen; i += testSegLen)
     {
@@ -1166,14 +1191,14 @@ static bool OSPI_flash_test(void *arg)
     }
 
 #ifdef OSPI_PROFILE
-    elapsedTime = TimerP_getTimeInUsecs() - startTime;
+    numTicks = Ospi_getGTCTimerTicks() - startTick;
 #if defined(FREERTOS)
     cpuLoad = LoadP_getCPULoad();
 #endif
-    /* calculate the write transfer rate in Kbps */
-    xferRate = (float) (((float) (testLen * 8)) / elapsedTime) * 1000U;
+    /* calculate the write transfer rate in KBps */
+    xferRate = (float) (((float) (testLen)) / OSPI_GTC_TICKS_TO_USEC(numTicks)) * 1000;
     xferRateInt = (uint32_t)xferRate;
-    SPI_log("\n Board_flashWrite %d bytes at transfer rate %d Kbps \n", testLen, xferRateInt);
+    SPI_log("\n Board_flashWrite %d bytes at transfer rate %u KBps \n", testLen, xferRateInt);
 #if defined(FREERTOS)
     SPI_log("\n Board_flashWrite CPU load %d%% \n", cpuLoad);
 #endif
@@ -1225,12 +1250,12 @@ static bool OSPI_flash_test(void *arg)
 #ifdef OSPI_PROFILE
     if( (test->dacMode) && (test->testId!=OSPI_TEST_ID_DAC_133M_SPI) && (test->testId!=OSPI_TEST_ID_WR_TUNING) && (test->testId!=OSPI_NAND_TEST_ID_DAC_133M_SPI))
     {
-        uint64_t    startPhyTuningTime;
-        uint64_t    elapsedPhyTuningTime;
+        uint64_t    startPhyTuningTick;
+        uint64_t    elapsedPhyTuningTicks;
         uint8_t     tmpRxBuf[4U];
 
         /* Dummy flash read to get just the PHY tuning time */
-        startPhyTuningTime = TimerP_getTimeInUsecs();
+        startPhyTuningTick = Ospi_getGTCTimerTicks();
         if(test->norFlash)
         {
             if (Board_flashRead(boardHandle, 0U, &tmpRxBuf[0], 4U, (void *)(&readMode)))
@@ -1240,14 +1265,14 @@ static bool OSPI_flash_test(void *arg)
                 goto err;
             }
         }
-        elapsedPhyTuningTime = TimerP_getTimeInUsecs() - startPhyTuningTime;
-        SPI_log("\n PHY tuning + 4 byte read took %d us \n", (uint32_t)elapsedPhyTuningTime);
+        elapsedPhyTuningTicks = Ospi_getGTCTimerTicks() - startPhyTuningTick;
+        SPI_log("\n PHY tuning + 4 byte read took %d us \n", (uint32_t)OSPI_GTC_TICKS_TO_USEC(elapsedPhyTuningTicks));
     }
 #if defined(FREERTOS)
     LoadP_reset( );
 #endif
     /* Get start time stamp for the read performance measurement */
-    startTime = TimerP_getTimeInUsecs();
+    startTick = Ospi_getGTCTimerTicks();
 #endif
     for (i = 0; i < testLen; i += testSegLen)
     {
@@ -1270,14 +1295,14 @@ static bool OSPI_flash_test(void *arg)
     }
 
 #ifdef OSPI_PROFILE
-    elapsedTime = TimerP_getTimeInUsecs() - startTime;
+    numTicks = Ospi_getGTCTimerTicks() - startTick;
 #if defined(FREERTOS)
     cpuLoad = LoadP_getCPULoad();
 #endif
-    /* calculate the write transfer rate in Kbps */
-    xferRate = (float) (((float) (testLen * 8)) / elapsedTime);
+    /* calculate the write transfer rate in KBps */
+    xferRate = (float) (((float) (testLen)) / OSPI_GTC_TICKS_TO_USEC(numTicks)) * 1000;
     xferRateInt = (uint32_t)xferRate;
-    SPI_log("\n Board_flashRead %d bytes at transfer rate %d Mbps \n", testLen, xferRateInt);
+    SPI_log("\n Board_flashRead %d bytes at transfer rate %u KBps \n", testLen, xferRateInt);
 #if defined(FREERTOS)
     SPI_log("\n Board_flashRead CPU load %d%% \n", cpuLoad);
 #endif
@@ -1450,6 +1475,13 @@ int main(void)
     Board_init(boardCfg);
 #endif
 
+#ifdef OSPI_PROFILE
+    /* Configure GTC Timer for profiling */
+    App_setGTCClk(OSPI_FLASH_GTC_MOD_ID,
+                           OSPI_FLASH_GTC_CLK_ID,
+                           OSPI_FLASH_GTC_CLK_FREQ);
+#endif
+
 #if defined(OSPI_TESTAPP_RTOS)
 
 	OS_init();
@@ -1529,3 +1561,153 @@ static void GeneratePattern(uint8_t *txBuf, uint8_t *rxBuf, uint32_t length)
     	*rxPtr++ = (uint8_t)0U;
     }
 }
+
+#ifdef OSPI_PROFILE
+static int32_t App_setGTCClk(uint32_t moduleId,
+                      uint32_t clkId,
+                      uint64_t clkRateHz)
+{
+    int32_t retVal;
+    uint64_t currClkFreqHz;
+
+    retVal = Sciclient_pmGetModuleClkFreq(moduleId,
+                                          clkId,
+                                          &currClkFreqHz,
+                                          SCICLIENT_SERVICE_WAIT_FOREVER);
+    if ((retVal == CSL_PASS) &&
+        (currClkFreqHz != clkRateHz))
+    {
+        retVal = OspiFlash_ClkRateSet(moduleId, clkId, clkRateHz);
+    }
+
+    /* Enable GTC */
+    HW_WR_REG32(CSL_GTC0_GTC_CFG1_BASE + 0x0U, 0x1);
+
+    return (retVal);
+}
+
+int32_t OspiFlash_ClkRateSet(uint32_t modId,
+                             uint32_t clkId,
+                             uint64_t clkRate)
+{
+    uint32_t i                          = 0U;
+    int32_t status                      = SPI_STATUS_SUCCESS;
+    int32_t finalStatus                 = SPI_STATUS_SUCCESS;
+    uint64_t respClkRate                = 0;
+    uint32_t numParents                 = 0U;
+    uint32_t moduleClockParentChanged   = 0U;
+    uint32_t clockStatus                = 0U;
+    uint32_t origParent                 = 0U;
+    uint32_t foundParent                = 0U;
+
+    /* Check if the clock is enabled or not */
+    status = Sciclient_pmModuleGetClkStatus(modId,
+                                            clkId,
+                                            &clockStatus,
+                                            SCICLIENT_SERVICE_WAIT_FOREVER);
+    if (status == CSL_PASS)
+    {
+        /* Get the number of parents for the clock */
+        status = Sciclient_pmGetModuleClkNumParent(modId,
+                                                   clkId,
+                                                   &numParents,
+                                                   SCICLIENT_SERVICE_WAIT_FOREVER);
+        if ((status == CSL_PASS) && (numParents > 1U))
+        {
+            status = Sciclient_pmGetModuleClkParent(modId, 
+                                                    clkId, 
+                                                    &origParent,
+                                                    SCICLIENT_SERVICE_WAIT_FOREVER);
+        }
+    }
+    if (status == CSL_PASS)
+    {
+        /* Disabling the clock */
+        status = Sciclient_pmModuleClkRequest(modId,
+                                              clkId,
+                                              TISCI_MSG_VALUE_CLOCK_SW_STATE_UNREQ,
+                                              0U,
+                                              SCICLIENT_SERVICE_WAIT_FOREVER);
+    }
+    if (status == CSL_PASS)
+    {
+        foundParent = 0U;
+        /* Try to loop and change parents of the clock */
+        for(i=0U;i<numParents;i++)
+        {
+            if (numParents > 1U)
+            {
+                /* Setting the new parent */
+                status = Sciclient_pmSetModuleClkParent(modId,
+                                                        clkId,
+                                                        clkId+i+1U,
+                                                        SCICLIENT_SERVICE_WAIT_FOREVER);
+                /* Check if the clock can be set to desirable freq. */
+                if (status == CSL_PASS)
+                {
+                    moduleClockParentChanged = 1U;
+                }
+            }
+            if (status == CSL_PASS)
+            {
+                status = Sciclient_pmQueryModuleClkFreq(modId,
+                                                        clkId,
+                                                        clkRate,
+                                                        &respClkRate,
+                                                        SCICLIENT_SERVICE_WAIT_FOREVER);
+            }
+            if ((status == CSL_PASS) && (respClkRate == clkRate))
+            {
+                foundParent = 1U;
+                break;
+            }
+        }
+    }
+    if ((status == CSL_PASS) && (numParents == 0U))
+    {
+        status = Sciclient_pmQueryModuleClkFreq(modId,
+                                                clkId,
+                                                clkRate,
+                                                &respClkRate,
+                                                SCICLIENT_SERVICE_WAIT_FOREVER);
+        if ((status == CSL_PASS) && (respClkRate == clkRate))
+        {
+            foundParent = 1U;
+        }
+    }
+    if (foundParent == 1U)
+    {
+        /* Set the clock at the desirable frequency*/
+        status = Sciclient_pmSetModuleClkFreq(modId,
+                                              clkId,
+                                              clkRate,
+                                              TISCI_MSG_FLAG_CLOCK_ALLOW_FREQ_CHANGE,
+                                              SCICLIENT_SERVICE_WAIT_FOREVER);
+    }
+    else
+    {
+        status = CSL_EFAIL;
+    }
+    if ((status == CSL_PASS) &&
+        (clockStatus == (uint32_t) TISCI_MSG_VALUE_CLOCK_SW_STATE_UNREQ))
+    {
+        /* Restore the clock again to original state */
+        status = Sciclient_pmModuleClkRequest(modId,
+                                              clkId,
+                                              clockStatus,
+                                              0U,
+                                              SCICLIENT_SERVICE_WAIT_FOREVER);
+    }
+    finalStatus = status;
+    if ((status != CSL_PASS) && (moduleClockParentChanged == 1U))
+    {
+        /* Setting the original parent if failure */
+        (void) Sciclient_pmSetModuleClkParent(modId,
+                                              clkId,
+                                              origParent,
+                                              SCICLIENT_SERVICE_WAIT_FOREVER);
+    }
+
+    return (finalStatus);
+}
+#endif
