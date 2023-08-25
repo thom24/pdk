c404b9b06ab53af4552e951f8d3fe7014d954fa7
Lohith Kumar <l-kumar@ti.com>
2023-08-25 18:02:47 +0530

    MCSPI: Fix Master Slave Test App
    The current MCSPI Master and Slave Test Apps keeping their
    startup codes at same location in OCMC_RAM which is leading
    to memory corruption. In order to fix this issue we have
    kept slave test apps OCMC_RAM start region at an offset
    which won't overlap with master test app startup code.
    
    Fixes: PDK-13308
    
    Signed-off-by: Lohith Kumar <l-kumar@ti.com>

---
+++ packages/ti/drv/spi/example/mcspi_slavemode/j721s2/slave_linker_append_r5f.cmd
@@ -0,0 +1,55 @@
+
+
+/*--------------------------------------------------------------*/
+/*                     Section Configuration                    */
+/*--------------------------------------------------------------*/
+SECTIONS
+{
+    .freertosrstvectors      : {} palign(8)      > VECTORS
+    .bootCode        : {} palign(8)      > MCSPI_SLAVE_OCMC_RAM
+    .startupCode     : {} palign(8)      > MCSPI_SLAVE_OCMC_RAM
+    .startupData     : {} palign(8)      > MCSPI_SLAVE_OCMC_RAM, type = NOINIT
+    .text            : {} palign(8)      > MCSPI_SLAVE_DDR0
+    GROUP {
+        .text.hwi    : palign(8)
+        .text.cache  : palign(8)
+        .text.mpu    : palign(8)
+        .text.boot   : palign(8)
+    }                                    > MCSPI_SLAVE_DDR0
+    .const           : {} palign(8)      > MCSPI_SLAVE_DDR0
+    .rodata          : {} palign(8)      > MCSPI_SLAVE_DDR0
+    .cinit           : {} palign(8)      > MCSPI_SLAVE_DDR0
+    .bss             : {} align(4)       > MCSPI_SLAVE_DDR0
+    .far             : {} align(4)       > MCSPI_SLAVE_DDR0
+    .data            : {} palign(128)    > MCSPI_SLAVE_DDR0
+    .sysmem          : {}                > MCSPI_SLAVE_DDR0
+    .data_buffer     : {} palign(128)    > MCSPI_SLAVE_DDR0
+    .bss.devgroup    : {*(.bss.devgroup*)} align(4)       > MCSPI_SLAVE_DDR0
+    .const.devgroup  : {*(.const.devgroup*)} align(4)     > MCSPI_SLAVE_DDR0
+    .boardcfg_data   : {} align(4)       > MCSPI_SLAVE_DDR0
+
+    /* USB or any other LLD buffer for benchmarking */
+    .benchmark_buffer (NOLOAD) {} ALIGN (8) > MCSPI_SLAVE_DDR0
+
+    .stack      : {} align(4)                               > MCSPI_SLAVE_DDR0  (HIGH)
+
+    .irqStack   : {. = . + __IRQ_STACK_SIZE;} align(4)      > MCSPI_SLAVE_DDR0  (HIGH)
+    RUN_START(__IRQ_STACK_START)
+    RUN_END(__IRQ_STACK_END)
+
+    .fiqStack   : {. = . + __FIQ_STACK_SIZE;} align(4)      > MCSPI_SLAVE_DDR0  (HIGH)
+    RUN_START(__FIQ_STACK_START)
+    RUN_END(__FIQ_STACK_END)
+
+    .abortStack : {. = . + __ABORT_STACK_SIZE;} align(4)    > MCSPI_SLAVE_DDR0  (HIGH)
+    RUN_START(__ABORT_STACK_START)
+    RUN_END(__ABORT_STACK_END)
+
+    .undStack   : {. = . + __UND_STACK_SIZE;} align(4)      > MCSPI_SLAVE_DDR0  (HIGH)
+    RUN_START(__UND_STACK_START)
+    RUN_END(__UND_STACK_END)
+
+    .svcStack   : {. = . + __SVC_STACK_SIZE;} align(4)      > MCSPI_SLAVE_DDR0  (HIGH)
+    RUN_START(__SVC_STACK_START)
+    RUN_END(__SVC_STACK_END)
+}
